b7ff22c70415c9f3605664f0fb67c9dd8a234ffd
RinatStr102 <tamam-102@mail.ru>
2024-12-01 14:47:11 +0300

    Добавил VacancyTest и VacancySteps c 3 тестами

--- src/test/java/vacancy/tests/VacancySteps.java
+++ src/test/java/vacancy/tests/VacancySteps.java
@@ -3,7 +3,7 @@ package vacancy.tests;
 import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class VacancySteps {
@@ -16,13 +16,26 @@ public class VacancySteps {
     // Поиск вакансии по запросу
     @Step("Ищем вакансию по запросу {query}")
     public void searchVacancies(String query) {
-        $("input[name='text']").setValue(query).pressEnter(); //Находим поле, вводим запрос и жмем Enter
+        $("input[name='q']").setValue(query).pressEnter(); //Находим поле, вводим запрос и жмем Enter
     }
 
     // Проверяем, что в результатах есть текст
     @Step("Проверяем, что результаты содержат текст {expectedText}")
     public void checkResults(String expectedText) {
-        $("selector").shouldHave(text(expectedText)); // Проверяем текст на странице результатов
+        $(".vacancy-card__title").shouldHave(text(expectedText)); // Проверяем текст на странице результатов
+    }
+
+    // Проверяем, что результатов нет
+    @Step("Проверяем, что результатов нет и отображается сообщение {expectedMessage}")
+    public void checkNoResults(String expectedMessage) {
+        $(".no-content__title").shouldHave(text(expectedMessage)); // проверяем текст "Поиск не дал результатов"
+    }
+
+    //Установка фильтра "Можно удаленно"
+    @Step("Устанавливаем фильтр 'Можно забрать'")
+    public void applyMiddleQualificationFilte() {
+        $(".select__input").click();
+        $("option[value='4']").shouldBe(visible).click();
     }
 
 }
--- src/test/java/vacancy/tests/VacancyTests.java
+++ src/test/java/vacancy/tests/VacancyTests.java
@@ -6,14 +6,36 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class VacancyTests {
+    VacancySteps step = new VacancySteps(); // Создаем объект для шагов
 
     @BeforeAll
-    public static void setup() {
-        Configuration.browser = "chrome";
+    static void setUp() {
+        Configuration.browser = "safari";
+        //Configuration.browserSize = "1920x1080";
     }
 
     @Test
-    @DisplayName("Проверка поиска вакансии по запросу 'Java разработчик'")
+    @DisplayName("Проверка поиска вакансии 'QA Engineer'")
+    public void searchQaVacancies() {
+        step.openSite("https://career.habr.com/"); //открыть сайт
+        step.searchVacancies("QA Engineer"); // ввести запрос
+        step.checkResults("QA Engineer"); // посмотреть результаты
+    }
 
+    @Test
+    @DisplayName("Проверка отсутствия результатов по некорректному запросу")
+    public void searchInvalidVacancy() {
+        step.openSite("https://career.habr.com/");
+        step.searchVacancies("Param-pam_pam");
+        step.checkNoResults("Поиск не дал результатов");
+    }
 
+    @Test
+    @DisplayName("Устанавливаем фильтр 'Средний (Middle)' в разделе квалификации")
+    public void searchQaVacanciesWithRemoteFilter() {
+        step.openSite("https://career.habr.com/");
+        step.searchVacancies("QA Engineer");
+        step.applyMiddleQualificationFilte();
+        step.checkResults("QA Engineer");
+    }
 }
